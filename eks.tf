db15078be49845a7b5a5da79c5317176713d352b
ayush2912 <76970440+ayush2912@users.noreply.github.com>
2024-06-06 23:46:03 +0530

    Update eks.tf

--- eks.tf
+++ eks.tf
@@ -66,17 +66,12 @@ resource "aws_security_group" "eks_node_group_sg" {
   vpc_id      = var.vpc_id
 
   ingress {
-    from_port   = 9443
-    to_port     = 9443
+    from_port   = 22
+    to_port     = 22
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
-  ingress {
-    from_port   = 443
-    to_port     = 443
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+
 }
 
 resource "aws_iam_role" "my_nodes" {
