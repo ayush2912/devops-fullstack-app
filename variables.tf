c18fc324a57df3e44c2f0dc67bd743d33a9ad002
ayush2912 <76970440+ayush2912@users.noreply.github.com>
2024-06-07 01:26:58 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,18 @@
+ variable "subnet_id_1" {
+  type = string
+  default = "subnet-0fe5eb410fea64832"
+ }
+ 
+ variable "subnet_id_2" {
+  type = string
+  default = "subnet-0369ab766192f2cb6"
+ }
+
+ variable "vpc_id" {
+  type = string
+  default = "vpc-069630328c95e70a5"
+ }
+ variable "subnet_ids" {
+  type    = list(string)
+  default = ["subnet-0fe5eb410fea64832", "subnet-0369ab766192f2cb6"]  
+}
