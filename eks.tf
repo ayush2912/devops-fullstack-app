d945eb41ee0cf0808d3d86e666928dee1e9d9b1a
ayush2912 <76970440+ayush2912@users.noreply.github.com>
2024-06-07 03:00:17 +0530

    Update eks.tf

--- eks.tf
+++ eks.tf
@@ -43,7 +43,7 @@ resource "aws_cloudwatch_log_group" "eks_cluster_logs" {
 resource "aws_eks_cluster" "my_cluster" {
   name     = "my-cluster"
   role_arn = aws_iam_role.my_cluster.arn
-  version  = "1.28"
+  version  = "1.29"
 
   vpc_config {
     subnet_ids = [var.subnet_id_1, var.subnet_id_2]
@@ -147,7 +147,7 @@ resource "aws_eks_addon" "coredns" {
 resource "aws_eks_addon" "kube_proxy" {
   cluster_name = aws_eks_cluster.my_cluster.name
   addon_name   = "kube-proxy"
-  addon_version = "v1.27.10-eksbuild.2"
+  addon_version = "v1.29.1-eksbuild.2"
 }
 
 resource "aws_eks_addon" "vpc_cni" {
