4c89bb50dbee64ab13fe0b7a2f4a198eec79a841
ayush2912 <76970440+ayush2912@users.noreply.github.com>
2024-06-17 19:21:03 +0530

    Update eks.tf

--- eks.tf
+++ eks.tf
@@ -47,6 +47,8 @@ resource "aws_eks_cluster" "my_cluster" {
 
     endpoint_public_access = true
     endpoint_private_access = false
+    endpoint_public_access_cidrs = ["0.0.0.0/0"]
+    
   }
  
   
