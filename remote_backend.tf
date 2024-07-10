1d05bf424dc203c6d1d7ad97f955f7ecd1137993
ayush2912 <76970440+ayush2912@users.noreply.github.com>
2024-07-11 01:15:58 +0530

    Update remote_backend.tf

--- remote_backend.tf
+++ remote_backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "my-fbp"
+    bucket         = "my-test-fbp"
     key            = "fbp/terraform.tfstate"
     region         = "us-east-1"
       
