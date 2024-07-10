0cd1563e3e984e7e558d083537ef06df0a2f1a6a
ayush2912 <76970440+ayush2912@users.noreply.github.com>
2024-07-11 01:38:01 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,20 +1,20 @@
  variable "subnet_id_1" {
   type = string
-  default = "subnet-0fe5eb410fea64832"
+  default = "subnet-080dd125349f6d200"
  }
  
  variable "subnet_id_2" {
   type = string
-  default = "subnet-0369ab766192f2cb6"
+  default = "subnet-0d4aa5966abe0e024"
  }
   variable "subnet_id_3" {
   type = string
-  default = "subnet-0e8dc6fbac55ef719"
+  default = "subnet-0572da41a4f5e6370"
  }
 
  variable "subnet_id_4" {
   type = string
-  default = "subnet-0631d574ba8cd4935"
+  default = "subnet-00730b45a14447204"
  }
 
  variable "vpc_id" {
